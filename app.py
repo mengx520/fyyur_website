53c7bfcd7fd5d61d28114ccb60528b84ea899643
mengx520 <mengx520@gmail.com>
2021-04-29 19:07:54 -0700

    Implement search
    https://docs.sqlalchemy.org/en/14/core/sqlelement.html#sqlalchemy.sql.expression.ColumnOperators.ilike

--- app.py
+++ app.py
@@ -240,6 +240,7 @@ def search_artists():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for 'A' should return 'Guns N Petals', 'Matt Quevado', and 'The Wild Sax Band'.
   # search for 'band' should return 'The Wild Sax Band'.
+  '''
   response={
     'count': 1,
     'data': [{
@@ -248,6 +249,13 @@ def search_artists():
       'num_upcoming_shows': 0,
     }]
   }
+  '''
+  data = Artist.query.filter(Artist.name.ilike("%" + request.form.get('search_term') + "%")).all()
+  response={
+    'count': len(data),
+    'data': data
+  }
+
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/artists/<int:artist_id>')
