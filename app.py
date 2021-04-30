fd9a79e733848eeb4dcc04c5a7b5e87154d37f9b
mengx520 <mengx520@gmail.com>
2021-04-29 19:13:23 -0700

    Add venue search

--- app.py
+++ app.py
@@ -135,13 +135,10 @@ def search_venues():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for Hop should return 'The Musical Hop'.
   # search for 'Music' should return 'The Musical Hop' and 'Park Square Live Music & Coffee'
+  data = Venue.query.filter(Venue.name.ilike("%" + request.form.get('search_term') + "%")).all()
   response={
-    'count': 1,
-    'data': [{
-      'id': 2,
-      'name': 'The Dueling Pianos Bar',
-      'num_upcoming_shows': 0,
-    }]
+    'count': len(data),
+    'data': data
   }
   return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
 
@@ -237,19 +234,9 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
+  # implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for 'A' should return 'Guns N Petals', 'Matt Quevado', and 'The Wild Sax Band'.
   # search for 'band' should return 'The Wild Sax Band'.
-  '''
-  response={
-    'count': 1,
-    'data': [{
-      'id': 4,
-      'name': 'Guns N Petals',
-      'num_upcoming_shows': 0,
-    }]
-  }
-  '''
   data = Artist.query.filter(Artist.name.ilike("%" + request.form.get('search_term') + "%")).all()
   response={
     'count': len(data),
