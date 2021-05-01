3254094cc14addaa970281d6b7193f7af7889d11
mengx520 <mengx520@gmail.com>
2021-05-01 00:05:08 -0700

    Fix __init__.py and update requirements

--- __init__.py
+++ __init__.py
@@ -1,11 +1,1 @@
-#----------------------------------------------------------------------------#
-# Launch.
-#----------------------------------------------------------------------------#
-import os
-from fyyur.app import create_app
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app = create_app()
-    app.run(host='0.0.0.0', port=port)
-
+# the fyyur module
